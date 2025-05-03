5eee5b9631e00ecc102580bb49c42d7fb0393f04
dima.moisieienko <dimonmail09@gmail.com>
2025-05-03 13:59:34 +0300

    handling errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,57 @@
 import { useState, useEffect } from "react";
 import "./index.css";
 
+const KEY = "db5a0200b7094f3ca2a141842250205";
+
 function App() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const res = await fetch(
+          `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}`
+        );
+        console.log(res);
+        // if(!res.ok){
+        //   throw new Error("error")
+        // }
+        const data = await res.json();
+        if(data.error){
+          setError(data.error.message)
+        }
+        setWeatherData(data);
+        setError(null);
+
+      } catch (err) {
+        console.log(err);
+        setError(err.message);
+        setWeatherData(null);
+      }
+    }
+    getData();
+  }, []);
+
+  console.log(weatherData);
   return (
     <div className="app">
       <div className="widget-container">
         <div className="weather-card-container">
           <h1 className="app-title">Weather Widget</h1>
           <div className="search-container">
-            <input type="text" placeholder="Enter city name" className="search-input" />
+            <input
+              type="text"
+              placeholder="Enter city name"
+              className="search-input"
+            />
           </div>
         </div>
         <div className="weather-card">
-          <h2>Moscow, Russia</h2>
+          <h2>
+            {weatherData?.location?.name}, {weatherData?.location?.country}
+          </h2>
           <img src="" alt="icon" className="weather-icon" />
           <p className="temperature">11°C</p>
           <p className="condition">rainy</p>
